b69a830a61528539cd520ed07a8cc82828136935
OkhammahkO <43776617+OkhammahkO@users.noreply.github.com>
2026-04-05 04:09:40 +0000

    new file:   brands/airhealth_for_hacs_ha/icon.png 	new file:   brands/airhealth_for_hacs_ha/icon@2x.png 	modified:   custom_components/airhealth/config_flow.py

--- custom_components/airhealth/config_flow.py
+++ custom_components/airhealth/config_flow.py
@@ -5,7 +5,8 @@ from typing import Any
 
 import voluptuous as vol
 from aiohttp import ClientError
-from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+from homeassistant.config_entries import ConfigFlow
+from homeassistant.data_entry_flow import ConfigFlowResult
 from homeassistant.helpers import selector
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
